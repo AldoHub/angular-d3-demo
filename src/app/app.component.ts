29bf54e83b0825dcc476a50156ecd22d5f0bb5aa
Aldo Caamal <aldocaava@hotmail.com>
2019-07-12 14:21:24 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { DataModel } from './models/data.model';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from "rxjs";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angulard3';
+  public data: Observable<DataModel>;
+
+  constructor(private httpClient: HttpClient) {
+    this.data = this.httpClient.get<DataModel>('./assets/data.json');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BarchartComponent } from './barchart/barchart.component';
+import { HttpClientModule } from "@angular/common/http"; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarchartComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule
   ],
   providers: [],
